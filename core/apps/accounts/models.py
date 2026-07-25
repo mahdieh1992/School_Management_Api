64401a5829ee6a32043267f609b703e7ba88112a
mahdieh1992 <mohamadimahdieh70@gmail.com>
2026-07-25 13:39:01 +0330

    create CustomUser model

--- core/apps/accounts/models.py
+++ core/apps/accounts/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+from django.core.validators import RegexValidator, MinLengthValidator
+from datetime import timedelta
+from django.utils import timezone
+from .managers import CustomeUserManager
 
 # Create your models here.
+
+def curent_date():
+    return timezone.now() + timedelta(days= 360 * 2)
+    
+
+class CustomUser(AbstractUser):
+    """
+        An abstract base class emplementing a fully feature for Custom User model
+        Email and password is required 
+    """
+    email = models.EmailField(_("email address"), blank=True, unique= True)
+    national_code = models.CharField(
+        _("National Code"),
+        max_length= 10,
+        unique= True,
+        validators=[
+            MinLengthValidator(10),
+            RegexValidator(
+                regex= r'^\d{10}$',
+                message= "National code must be exactly 10 digits"
+            )
+        ])
+    
+    date_expired = models.DateTimeField(_("Date Expired"), default=curent_date)
+    is_registered = models.BooleanField(default= False)
+    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    
+    objects = CustomeUserManager()
+    
+    def __str__(self):
+        return self.email
+        
--- core/core/settings.py
+++ core/core/settings.py
@@ -13,9 +13,10 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from decouple import config
 from pathlib import Path
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -121,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+AUTH_USER_MODEL = "accounts.CustomUser"
