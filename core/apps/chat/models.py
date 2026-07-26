bd08bafdabaf54b353c31830e19251acf8b0572a
mahdieh1992 <mohamadimahdieh70@gmail.com>
2026-07-26 15:42:49 +0330

    add message, message_receiver,news, news_receiver models

--- core/apps/chat/models.py
+++ core/apps/chat/models.py
@@ -1,3 +1,37 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+user = get_user_model()
+
+class Message(models.Model):
+    """
+    Represents a message sent by a user.
+    """
+    text = models.TextField(blank= True, null= True)
+    sender = models.ForeignKey(user, on_delete= models.CASCADE, related_name="message")
+    created_at = models.DateTimeField(auto_now_add= True)
+    
+    class Meta:
+        verbose_name = "Message"
+        verbose_name_plural = "Messages"
+        
+    def __str__(self):
+        return self.sender.email
+    
+    
+class MessageReceiver(models.Model):
+    """
+        Tracks the recipients of a message and their read status.
+    """
+    message = models.ForeignKey(Message, on_delete= models.CASCADE, related_name="message_receiver")
+    receiver = models.ForeignKey(user, on_delete= models.CASCADE, related_name="message_receiver")
+    is_read = models.BooleanField(default= False)
+    read_date = models.DateTimeField(blank= True, null= True)
+    
+    class Meta:
+        verbose_name = "Message Receiver"
+        verbose_name_plural = "Messages Receivers"
+        
+    def __str__(self):
+        return self.receiver.email
+    
--- core/apps/news/models.py
+++ core/apps/news/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from ..school.models import ClassRoom, BaseModel
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+user = get_user_model()
+class News(BaseModel):
+    """
+        News model for each class 
+    """
+    title = models.CharField(max_length= 150, help_text="Title of new")
+    body = models.TextField(blank= True, null= True)
+    created_by = models.ForeignKey(user, on_delete= models.CASCADE, related_name= "news")
+    class_room = models.ForeignKey(ClassRoom, on_delete= models.CASCADE, related_name="news")
+    
+    class Meta:
+        verbose_name = "New"
+        verbose_name_plural = "News"
+        
+    def __str__(self):
+        return self.title
+    
+    
+class NewsReceiver(BaseModel):
+    """
+    Represent news receiver model for each class
+    """
+    news = models.ForeignKey(News, on_delete= models.CASCADE, related_name="receiver")
+    student = models.ForeignKey(user, on_delete=models.CASCADE, related_name="receiver")
+    is_read = models.BooleanField(default= False)
+    read_date= models.DateTimeField(blank= True, null= True)
+    
+    class Meta:
+        verbose_name = "News Receiver"
+        verbose_name_plural = "News Receivers"
+        
+    def __str__(self):
+        return f"{self.student.email} - {self.news.title}"
+    
