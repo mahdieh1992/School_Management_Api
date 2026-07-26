158b1b9ef98813ba0be48137ab1319334c049498
mahdieh1992 <mohamadimahdieh70@gmail.com>
2026-07-26 18:27:12 +0330

    config admin for user and profile model

--- core/apps/accounts/admin.py
+++ core/apps/accounts/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Profile
+from django.utils.translation import gettext_lazy as _
 # Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    list_display = ("email", "is_staff", "is_active","is_registered")
+    list_filter = ("is_staff", "is_active", "is_registered")
+    search_fields = ("email", "national_code", "first_name", "last_name")
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal_info"), {"fields": ("first_name", "last_name", "national_code")}),
+        (_("Permissions"), {"fields": ("is_staff", "is_active", "is_superuser", "is_registered", "groups", "user_permissions")})
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ( "email", "password1", "password2", "is_staff",
+                "is_active", "groups", "user_permissions")
+        }),
+    )
+    
+    search_fields = ("email",)
+    
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ("user__email", "mobile_number","bio")
--- core/apps/accounts/models.py
+++ core/apps/accounts/models.py
@@ -5,7 +5,8 @@ from django.core.validators import RegexValidator, MinLengthValidator
 from datetime import timedelta
 from django.utils import timezone
 from .managers import CustomeUserManager
-
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 # Create your models here.
 
 def curent_date():
@@ -50,7 +51,7 @@ class Profile(models.Model):
         female = "0", "Female"
         men = "1", "Men"
         
-    user = models.OneToOneField(CustomUser, on_delete= models.CASCADE, related_name="profile")
+    user = models.OneToOneField(CustomUser, on_delete= models.CASCADE)
     mobile_number = models.CharField(
         max_length= 11,
         blank= True,
@@ -65,9 +66,17 @@ class Profile(models.Model):
     gender= models.CharField(max_length=1 , choices= GenderChoice, default=GenderChoice.female)
     image = models.ImageField(upload_to='profile_images/', blank= True, null= True)
     bio = models.TextField(blank= True, null= True)
-    latitude = models.DecimalField(max_digits= 8, decimal_places= 3)
-    longitude = models.DecimalField(max_digits= 8, decimal_places= 3)
+    latitude = models.DecimalField(max_digits= 8, decimal_places= 3, blank= True, null= True)
+    longitude = models.DecimalField(max_digits= 8, decimal_places= 3, blank= True, null= True)
     
     def __str__(self):
         return self.user.email
         
+@receiver(post_save, sender= CustomUser)    
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+# @receiver(post_save, sender= CustomUser)  
+# def save_user_profile(sender, instance, **kwargs):
+#     instance.profile.save()
