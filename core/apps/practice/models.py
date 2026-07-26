6865ba62b390053a62aa5285ce91ba5b48acec15
mahdieh1992 <mohamadimahdieh70@gmail.com>
2026-07-26 13:38:05 +0330

    add model practice, practice answer and class

--- core/apps/practice/models.py
+++ core/apps/practice/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from ..school.models import BaseModel, School, ClassRoom
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 # Create your models here.
+class Practice(BaseModel):
+    """     
+        Determin practices for students of the class 
+    """
+    title = models.CharField(max_length=150, help_text="title of practice")
+    body = models.TextField(blank= True, null= True)
+    deadline = models.DateTimeField()
+    attachment = models.FileField(upload_to="upload_practice/", blank= True, null= True)
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name="practice")
+    class_room = models.ForeignKey(ClassRoom, on_delete=models.CASCADE, related_name="practice")
+    
+    class Meta:
+        verbose_name= "Practice"
+        verbose_name_plural = "Practices"
+        
+    def __str__(self):
+        return f"{self.title}-{self.created_by}"
+    
+# class PracticeAnswer(BaseModel):
+#     """
+#      Representaion practice answers 
+#     """
+#     practice = models.ForeignKey(Practice, on_delete= models.CASCADE, related_name="practice_answer")
+#     user = models.ForeignKey(User, on_delete= models.CASCADE, related_name="practice_answer")
+#     answer = models.TextField(blank= True, null= True)
+#     attachment = models.FileField(upload_to="upload_practice/")
+    
+#     class Meta:
+#         verbose_name = "Practice Answer"
+#         verbose_name_plural = "Practices Answer"
+    
+#     def __str__(self):
+#         return f"{self.practice.title[:10]}-{self.user}"
+    
--- core/apps/school/models.py
+++ core/apps/school/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
+User = get_user_model()
+
+class BaseModel(models.Model):
+    """
+        it is used to all model that want to use a base model
+    """
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default= True, blank= True, help_text= "This object is active")
+    
+    class Meta:
+        abstract= True
+        
+class School(BaseModel):
+    """
+        School model for storing school information
+    """
+    name = models.CharField(max_length= 150, help_text= "school name")
+    latitude = models.DecimalField(max_digits=8, decimal_places=3)
+    longitude = models.DecimalField(max_digits=8, decimal_places=3)
+    
+    class Meta:  
+        verbose_name = "school"
+        verbose_name_plural= "schools"
+    
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        self.name= self.name.strip().title()
+        return super().save(*args, **kwargs)
+    
+    
+
+class Lesson(BaseModel):
+    """ 
+        Lesson model is used to storing lessons inforamtion
+    """
+    title = models.CharField(max_length= 150, help_text="Lesson name")
+    code = models.IntegerField(unique= True)
+    
+    class Meta:
+        verbose_name = "Lesson"
+        verbose_name_plural= "Lessons"
+    
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        self.code += 5
+        self.title = self.title.strip().title()
+        return super().save(*args, **kwargs)
+    
+class ClassRoom(BaseModel):
+    """
+        Represents a classroom for a lesson in a school.
+    """
+    name = models.CharField(max_length= 150, help_text= "Class name")
+    school = models.ForeignKey(School, on_delete= models.CASCADE, related_name="class_rooms")
+    lesson = models.ForeignKey(Lesson, on_delete= models.CASCADE, related_name="class_rooms")
+    teacher = models.ForeignKey(User, on_delete= models.CASCADE, related_name="teaching_classes")
+    student = models.ManyToManyField(User, related_name="enrolled_classes")
+    
+    class Meta:
+        verbose_name= "Class"
+        verbose_name_plural= "Classes"
+    
+    def __str__(self):
+        return self.name
+    
